f220dd8f8d035cb8bebf4f6bc10081520e6b2b25
픽셀강박 <126048017+denshol@users.noreply.github.com>
2024-01-17 00:59:27 +0900

    Title: 정사각형의 넓이, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240117/정사각형의 넓이/area-of-a-rectangle.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split(" ");
+
+let n = Number(input[0]);
+
+
+
+console.log(n*n);
+
+if(n<5)
+{
+
+console.log(tiny); 
+}
