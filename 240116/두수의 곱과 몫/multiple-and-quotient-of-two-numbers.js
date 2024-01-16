ffbc9cf601a43bb61f14e9fc006417d1c12a16c7
픽셀강박 <126048017+denshol@users.noreply.github.com>
2024-01-16 23:04:40 +0900

    Title: 두수의 곱과 몫, Time: 182ms, Memory: 14MB, Status: Passed - CodeTree

--- 240116/두수의 곱과 몫/multiple-and-quotient-of-two-numbers.js
+++ 240116/두수의 곱과 몫/multiple-and-quotient-of-two-numbers.js
@@ -5,4 +5,4 @@ input[0] = Number(input[0]);
 input[1] = Number(input[1]);
 
 
-console.log(`${input[0]} * ${input[1]} = ${input[0]*input[1]}\n${input[0]} / ${input[1]} = ${input[0]/input[1]}`);
+console.log(`${input[0]} * ${input[1]} = ${input[0]*input[1]}\n${input[0]} / ${input[1]} = ${parseInt(input[0]/input[1])}`);
