6ee32acb6e3197c655812f3dddb369ceacb54485
픽셀강박 <126048017+denshol@users.noreply.github.com>
2024-01-17 14:16:00 +0900

    Title: 홀수만 출력하기, Time: 183ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240117/홀수만 출력하기/print-only-odd-numbers.js
+++ 240117/홀수만 출력하기/print-only-odd-numbers.js
@@ -5,6 +5,10 @@ let input = fs.readFileSync(0).toString().split("\n");
 
 let res="";
 for(let i=0;i<input.length;i++){
+
+    let x = Number(input[i]);
+
+
     if(input[i]%2!=0 && input[i]%3==0){
       console.log(input[i]);
     }
