f759fdcc311b1049acdd160f82f7a4ca5fe790d3
픽셀강박 <126048017+denshol@users.noreply.github.com>
2024-01-17 10:07:20 +0900

    Title: 굉장한 숫자, Time: 194ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 240117/굉장한 숫자/amazing-number.js
@@ -0,0 +1,26 @@
+// 입력 및 변수 선언
+const fs = require("fs");
+let a = Number(fs.readFileSync(0).toString().trim());
+
+// 출력
+
+
+
+if (a % 2 != 0) {
+
+    if(a%3==0){
+
+    console.log("true");
+    }else{
+
+    console.log("false");
+    }
+
+}
+else {
+    if(a%5==0){
+        console.log("true")
+    }else{
+        console.log("false");
+    }
+}
