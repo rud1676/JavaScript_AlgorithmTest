a3bffcc8ba744387019e6a1e8467194172f76ace
픽셀강박 <126048017+denshol@users.noreply.github.com>
2024-01-17 11:15:11 +0900

    Title: 일 수 구하기, Time: 206ms, Memory: 14MB, Status: Passed - CodeTree

--- 240117/일 수 구하기/number-of-days-in-month.js
+++ 240117/일 수 구하기/number-of-days-in-month.js
@@ -5,7 +5,7 @@ let input = Number(fs.readFileSync(0).toString().trim());
 let days;
 
 switch(input){
-    case 1: case 3: case 4: case 5: case 7: case 8: case 10: case 12:
+    case 1: case 3: case 5: case 7: case 8: case 10: case 12:
     days = 31; break;
 
     case 4: case 6: case 9: case 11:
