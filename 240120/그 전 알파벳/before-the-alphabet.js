54d0dfe5c451d6806c19df9ab735c7b5d027ca35
픽셀강박 <126048017+denshol@users.noreply.github.com>
2024-01-20 23:03:24 +0900

    Title: 그 전 알파벳, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240120/그 전 알파벳/before-the-alphabet.js
+++ 240120/그 전 알파벳/before-the-alphabet.js
@@ -6,5 +6,10 @@ let input = fs.readFileSync(0).toString().trim();
 var str = input;
 
 var num = str.charCodeAt()-1;
+if(input==='a'){
+console.log(z);
+}else{
+
 
 console.log(String.fromCharCode(num));
+}
