2ba422c001294703114854243624918359def365
픽셀강박 <126048017+denshol@users.noreply.github.com>
2024-01-20 17:01:28 +0900

    Title: 특정 문자로 시작하는 문자열, Time: 201ms, Memory: 14MB, Status: Passed - CodeTree

--- 240120/특정 문자로 시작하는 문자열/strings-that-start-with-a-specific-character.js
+++ 240120/특정 문자로 시작하는 문자열/strings-that-start-with-a-specific-character.js
@@ -4,15 +4,15 @@ let input = fs.readFileSync(0).toString().trim().split("\n");
 
 let char = input[input.length-1];
 
-let n = input[0];
+let n = Number(input[0]);
 
 var cnt = 0;
 var sum =0;
-for(let i=1;i<n;i++){
+for(let i=1;i<=n;i++){
     if(input[i][0]===char){
         cnt++;
         sum+= input[i].length;
     }
 }
 
-console.log(`${cnt} ${(sum/cnt).toFixed(2)}`);
+console.log(cnt,(sum/cnt).toFixed(2));
