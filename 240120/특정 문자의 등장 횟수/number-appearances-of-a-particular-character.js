aa96095da582f8d2ea7bb157442b2ea9dfc1f41a
픽셀강박 <126048017+denshol@users.noreply.github.com>
2024-01-20 18:11:29 +0900

    Title: 특정 문자의 등장 횟수, Time: 181ms, Memory: 14MB, Status: Passed - CodeTree

--- 240120/특정 문자의 등장 횟수/number-appearances-of-a-particular-character.js
+++ 240120/특정 문자의 등장 횟수/number-appearances-of-a-particular-character.js
@@ -10,16 +10,19 @@ let cntB=0;
 
 
 let res;
-for(let i=0;i<input.length;i++){}
-// 문자열 'ee'가 존재하는지 확인하고, 존재하면 Yes, 아니면 No를 출력합니다.
-if (input.includes("ee")) {
+for(let i=0;i<input.length;i++){
+if (input.slice(i,i+2)===("ee")) {
    cntA++;
 }
+if (input.slice(i,i+2)===("eb")) {
+    cntB++;
+} 
+}
+// 문자열 'ee'가 존재하는지 확인하고, 존재하면 Yes, 아니면 No를 출력합니다.
+
 
 
 // 문자열 'ab'가 존재하는지 확인하고, 존재하면 Yes, 아니면 No를 출력합니다.
-if (input.includes("eb")) {
-    cntB++;
-} 
+
 
 console.log(`${cntA} ${cntB}`);
