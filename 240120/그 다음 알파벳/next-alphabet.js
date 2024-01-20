2ca22e53c99ddbb612d846ae29e6d15923cbeb28
픽셀강박 <126048017+denshol@users.noreply.github.com>
2024-01-20 22:55:27 +0900

    Title: 그 다음 알파벳, Time: 171ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240120/그 다음 알파벳/next-alphabet.js
+++ 240120/그 다음 알파벳/next-alphabet.js
@@ -3,6 +3,11 @@ const fs = require("fs");
 let input = fs.readFileSync(0).toString().trim();
 
 
-console.log(`${String.fromCharCode(Number(input[0].charCodeAt())+1)} `);
+var str = String.fromCharCode(Number(input[0].charCodeAt())+1);
+if(String.fromCharCode(Number(input[0].charCodeAt())+1)==='z'){
+str='a';
+}
+
+console.log(str);
 
 //input[0] = input[0].split(" ");
