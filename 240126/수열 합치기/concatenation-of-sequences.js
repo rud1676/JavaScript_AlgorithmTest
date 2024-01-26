c308a3fdcd7e6fa0e626f6f010e7f11a5124036b
픽셀강박 <126048017+denshol@users.noreply.github.com>
2024-01-26 17:58:13 +0900

    Title: 수열 합치기, Time: 515ms, Memory: 49MB, Status: Passed - CodeTree

--- 240126/수열 합치기/concatenation-of-sequences.js
+++ 240126/수열 합치기/concatenation-of-sequences.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 
 let input = fs.readFileSync(0).toString().trim().split("\n");
 
-let arr = input[0].split(" ").map(Number);
+//let arr = input[0].split(" ").map(Number);
 
 let brr = input[1].split(" ").map(Number);
 
@@ -10,8 +10,8 @@ let crr = input[2].split(" ").map(Number);
 
 let drr= [...crr,...brr];
 
-drr=drr.filter((v,i)=> {
-   return i===drr.indexOf(v)});
+// drr=drr.filter((v,i)=> {
+//    return i===drr.indexOf(v)});
 
 
 console.log(drr.sort((a,b)=>a-b).join(" "))
