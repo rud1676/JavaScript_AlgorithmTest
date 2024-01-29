b2aff0404514f28acf7bacd152368a42d32db18f
픽셀강박 <126048017+denshol@users.noreply.github.com>
2024-01-29 21:56:42 +0900

    Title: 나눗셈의 나머지, Time: 255ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240129/나눗셈의 나머지/remainder-of-division.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+
+let input = fs.readFileSync(0).toString().trim().split(" ").map(Number);
+
+let a  = input[0];
+let b = input[1];
+let num = 0;
+let sum =0;
+let arr=new Array(b).fill(0);
+
+
+while(a>1){
+
+a=Math.floor(a/b);
+num=a%b;
+arr[num]++;
+
+
+}
+for(let i=0;i<arr.length;i++){
+    sum+=arr[i]**2;
+}
+
+console.log(sum);
